92e82a97cf98e2461023bab7604425c401ad7cee
Ferran <marvenferry60@gmail.com>
2023-12-22 09:22:31 +0100

    Changing js to jsx

---
+++ zenproject/src/app/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home page!</h1>
+            <p>This is the default page for your React application.</p>
+        </div>
+    );
+}
+
+export default Home;
--- zenproject/src/app/page.js
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main>
-      <h1>Buenas tardes</h1>
-    </main>
-  )
-}
