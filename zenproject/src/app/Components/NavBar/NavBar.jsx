e229b3ed94844fa2be9b9ec88eeb1c142c87e6eb
Ferran <marvenferry60@gmail.com>
2024-02-26 21:35:13 +0100

    Few things

--- zenproject/src/app/Components/NavBar/NavBar.jsx
+++ zenproject/src/app/Components/NavBar/NavBar.jsx
@@ -3,12 +3,14 @@ import NavBarcss from './NavBar.css'
 
 export const NavBar = () => {
   return (
-    <nav id='navBar'>
-      <a className='navBar-component'><p>Tareas</p></a>
-      <a className='navBar-component'><p>Calendario</p></a>
-      <a className='navBar-component'><p>Playlist</p></a>
-      <a className='navBar-component'><p>Graficos</p></a>
-      <a className='navBar-component'><img src=''></img></a> {/* Poner el avatar del usuario */}
-    </nav>
+    <>
+      <nav id='navBar'>
+        <a className='navBar-component'><p>Tareas</p></a>
+        <a className='navBar-component'><p>Calendario</p></a>
+        <a className='navBar-component'><p>Playlist</p></a>
+        <a className='navBar-component'><p>Graficos</p></a>
+        <a className='navBar-component'><img src=''></img></a> {/* Poner el avatar del usuario */}
+      </nav>
+    </>
   )
 }
--- zenproject/src/app/page.jsx
+++ zenproject/src/app/page.jsx
@@ -1,5 +1,6 @@
 import { Home } from "./Pages/Home";
 import Fonts from './css/Fonts/Fonts.css'
+import GlobalCss from './css/Global.css'
 
 export default function App() {
   return (
