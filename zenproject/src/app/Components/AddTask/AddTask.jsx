381e9fa636363c1a1fd6afd2625e42225247cfe5
Ferran <marvenferry60@gmail.com>
2024-02-26 21:19:41 +0100

    Adding add Task

---
+++ zenproject/src/app/Components/AddTask/AddTask.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AddTask = () => {
+  return (
+    <div id='addTaskContainer'>
+      <p>+ Añadir tarea</p>
+    </div>
+  )
+}
--- zenproject/src/app/Pages/Home.jsx
+++ zenproject/src/app/Pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { NavBar } from '../Components/NavBar/NavBar';
+import { AddTask } from '../Components/AddTask/AddTask';
+
 export const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <AddTask></AddTask>
         </div>
     )
 }
